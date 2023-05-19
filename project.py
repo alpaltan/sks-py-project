d837ffa86d3fedb19fb28aa59d1d554ffb8a1a45
alpaltan <alpaltnceng@gmail.com>
2023-05-19 06:38:45 +0300

    son duzeltmeler yapildi

--- project.py
+++ project.py
@@ -2,30 +2,33 @@ import random
 import time
 
 levelList = [30, 50, 100, 200, 400]
-seviye = 1
 can = 10
-print("***********************************")
-print("Sayi Tahmin Oyununa Hos Geldin!!!\n")
+print("Sayi Tahmin Oyununa Hos Geldin!!!")
+print("***********************************\n")
 print("Oyunumuzda 5 adet seviye bulunuyor")
 print("1 ile 30, 50, 100, 200 ve 400 ")
 print("Arasindaki sayilari canin bitmeden dogru tahmin etmen gerekiyor!")
 print("Her yanlis tahmininde 1 can kaybedersin!")
-print("Dogru tahmininde ise 2 can kazanirsin!")
-print(*"SIMDIDEN BOL SANS")
-print("***********************************\n")
+print("Dogru tahmininde ise 2 can kazanirsin!\n")
+print("***********************************")
+print(*"SIMDIDEN BOL SANS\n")
 for i in range(0, 5, 1):
     tepeDeger = levelList[i]
     randomSayi = random.randint(1, tepeDeger)
+    if (can == 0):
+        print("Uzgunum canin Bitti... :(")
+        break
+    print("Seviye: " + str(i+1))
     while can != 0:
         girilenDeger = int(
-            input("1 ile " + str(tepeDeger) + " arasinda bir sayi tut"))
+            input("1 ile " + str(tepeDeger) + " arasinda bir deger tahmin etme sirasi:"))
         if (girilenDeger == randomSayi):
-            print("buldun")
+            print("Tebrik Ederimm!!!\n2 can eklendi...")
             can += 2
             break
         elif (girilenDeger < (randomSayi)):
             can -= 1
-            print("Tuttugum sayi tahmininden daha buyuk")
+            print("Tuttugum sayi tahmininden daha BUYUK! | Kalan Can: " + str(can))
         elif (girilenDeger > (randomSayi)):
             can -= 1
-            print("Tuttugum sayi tahmininden daha kucuk")
+            print("Tuttugum sayi tahmininden daha KUCUK! | Kalan Can: " + str(can))
